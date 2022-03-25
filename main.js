121e0e895895e67e2096ff4519736bb636bb44a1
caps-lock23 <obnamiacada@gmail.com>
2022-03-25 16:11:35 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+//hour
+//if hour is between 1am to 12am: goodmorning
+//if hour is between 12am to 6pm: goodafternoon
+//if hour is between 6pm to 12pm: goodevening
+
+if (condition) {
+  statement;
+} else if (anotherCondition) {
+  statement;
+} else if (anotherCondition) {
+  statement;
+} else {
+  statement;
+}
